087110ac7abbb46e276e7e76c6e4ea39a2533017
corly74 <55600392+corly74@users.noreply.github.com>
2021-06-24 15:39:20 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import Table from './containers/table';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Table />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
