0cb7f1b221e941ad9cf89011f22bd500c3cf451c
corly74 <55600392+corly74@users.noreply.github.com>
2021-06-25 14:21:51 +0800

    Update table.js

--- containers/table.js
+++ containers/table.js
@@ -1,57 +1,88 @@
 import '@coreui/coreui/dist/css/coreui.min.css';
-// import {
-//   CDataTable, CTable, CCard, CContainer, CButton, CCardBody, CTableHead, CTableRow, CTableHeaderCell,
-//   CTableDataCell, CTableBody,
-// } from '@coreui/react';
-// import React, { useState } from 'react';
-import DataTable from 'react-data-table-component';
-import React, { memo, useEffect, useState } from 'react';
 
-
-const data = [{ ID: 1801387266, name: 'Conan the Barbarian', desc:'SAQ-200',type:'general'},
-              { ID: 1801387266, name: 'Conan the Barbarian', desc:'SAQ-200',type:'general'}                                                    ];
-const columns = [
-  {
-    name: 'ID',
-    selector: 'ID',
-    sortable: true,
-  },
-  {
-    name: 'Name',
-    selector: 'name',
-    sortable: true,
-  },
-
-  {
-    name: 'Desc',
-    selector: 'desc',
-    sortable: true,
-    right: true,
-  },
-  {
-    name: 'Type',
-    selector: 'type',
-    sortable: true,
-    right: true,
-  },
-];
+import React,{useEffect} from 'react'
+import {
+  CBadge,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+ 
+} from '@coreui/react';
+import { useSelector, useDispatch } from 'react-redux';
+import {LOAD_TABLE} from './constants';
+import {loadTable} from './action';
 
 
 
-const Table = () => {
-  return (
-    <div className="container mt-5">
-      <DataTable
-        title="Arnold Movies"
-        columns={columns}
-        data={data}
-      />
 
 
 
+
+
+const getBadge = status => {
+  switch (status) {
+    case 'Active': return 'success'
+    case 'Inactive': return 'secondary'
+    case 'Pending': return 'warning'
+    case 'Banned': return 'danger'
+    default: return 'primary'
+  }
+}
+
+const fields = ['No','name','desc', 'county', 'type','updateTime']
+
+
+
+
+
+
+
+
+
+const Table = () => { 
+
+  const projectState = useSelector(state => state.projectList);
+  console.log(projectState);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({type:LOAD_TABLE,abc:'123'});
+  }, []);
+  return ( 
+    <div className="container mt-5"> 
+      <CCol xs="12" lg="6">
+          <CCard>
+            <CCardHeader>
+            <h3>Get data from ChtIiot</h3>
+            </CCardHeader>
+            <CCardBody>
+            <CDataTable
+              items={projectState}
+              fields={fields}
+              striped
+              itemsPerPage={5}
+              pagination
+              scopedSlots = {{
+                'No':
+                  (item)=>{
+                    return( <td>
+                    {projectState.indexOf(item)+1}
+                    </td>)
+                   
+                 }
+
+                 
+
+              }}
+            />
+            </CCardBody>
+          </CCard>
+        </CCol>
     </div>
   )
-};
+
+}
 
 
 export default Table;
